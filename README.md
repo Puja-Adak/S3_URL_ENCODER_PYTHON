80d7718edf017af7c745746fd719f50b574ee82a
Puja-Adak <111103095+Puja-Adak@users.noreply.github.com>
2022-08-12 14:37:55 +0800

    2nd Push

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # S3_URL_ENCODER_PYTHON
+
+
+Encode url symbols to be s3 compatible in Python
+
+
+## Code of conduct
+
+Code of conduct can be found at [CODE_OF_CONDUCT.md](CODE_OF_CONDUCT.md)
+
+## Contributing
+
+Make sure to read **Contribution Guidelines** at [CONTRIBUTING.md](CONTRIBUTING.md) before contributing.
+
+## License & Copyright
+
+Copyright (C) 2022 Puja Adak
+
+Licensed under the [MIT LICENSE](LICENSE)
+
+## Contributors
+
+Puja Adak , Anil Manohar and Arijit Ojha
--- Tests/test.py
+++ Tests/test.py
@@ -1,9 +1,9 @@
 # importing sys
 import sys
  # adding Folder_2 to the system path
-sys.path.insert(0, 'C:\Github\S3_URL_DECODER_PYTHON\Source')
+sys.path.insert(0, 'C:\Github\S3_URL_ENCODER_PYTHON\SOurce')
 
-from S3Decoder import s3decode
+from S3Encoder import s3Encode
 
 #S3 bucket structure {my_bucket_name}\folder name \filename
 
@@ -13,26 +13,26 @@ from S3Decoder import s3decode
 #test case 1
 
 S3_keyname = "input\Z00010246+%281%29.PNG"
-print(s3decode(S3_keyname))
+print(s3Encode(S3_keyname))
 # output 
-#input\Z00010246 (1).PNG
+#input\Z00010246%2B%281%29.PNG
 
 
 #test case 2
 
 S3_keyname = "input\Z00010246 %281%29.PNG"
-print(s3decode(S3_keyname))
-#output = input\Z00010246 (1).PNG
+print(s3Encode(S3_keyname))
+#output = input\Z00010246%2B%281%29.PNG
 
 #test case 3
 
 S3_keyname = "input\Z00010246.png"
-print(s3decode(S3_keyname))
+print(s3Encode(S3_keyname))
 #output = input\Z00010246.png
 
 
 #test case 4
 
 S3_keyname = "input\Z00010246 %28-1%29.png"
-print(s3decode(S3_keyname))
-#output = input\Z00010246 (-1).png
+print(s3Encode(S3_keyname))
+#output = input\Z00010246%2B%28%E2%801%29.png
