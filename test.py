c2291a7d038801100d9b7b7d5ae4dd296e02099f
Puja-Adak <111103095+Puja-Adak@users.noreply.github.com>
2022-08-12 11:54:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,33 @@
-import S3Decoder. as S3Decoder
+import S3Decoder as S3Decoder
+
+#S3 bucket structure {my_bucket_name}\folder name \filename
+
+# key is "folder name \filename" to download the S3 file 
+
+
+#test case 1
 
 S3_keyname = "input\Z00010246+%281%29.PNG"
-
-S3Decoder.s3decode(S3_keyname)
-
+print(S3Decoder.s3decode(S3_keyname))
 # output 
 #input\Z00010246 (1).PNG
+
+
+#test case 2
+
+S3_keyname = "input\Z00010246 %281%29.PNG"
+print(S3Decoder.s3decode(S3_keyname))
+#output = input\Z00010246 (1).PNG
+
+#test case 3
+
+S3_keyname = "input\Z00010246.png"
+print(S3Decoder.s3decode(S3_keyname))
+#output = input\Z00010246.png
+
+
+#test case 4
+
+S3_keyname = "input\Z00010246 %28-1%29.png"
+print(S3Decoder.s3decode(S3_keyname))
+#output = input\Z00010246 (-1).png
